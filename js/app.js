f5a44a8c057fb0199e32bf18c25917182ad95e79
ivan.janson <ivan.janson@gmail.com>
2016-07-03 21:42:04 +0300

    Научились возвращаться по нажатию кнопки Назад. А также запоминаем в state выбраную клинику

--- js/app.js
+++ js/app.js
@@ -2,48 +2,89 @@ window.ee = new EventEmitter();
 
 
 var Nav = React.createClass({
+  backButtonClick: function(e) {
+    e.preventDefault();
+    ee.emit('Buttons.back', {});
+  },
   render: function() {
     return(
         <div id="nav">
-          <div id="back" className="hide">Назад</div>
+          <div id="back"
+               onClick={this.backButtonClick}
+               className={this.props.pageNum > 1 ? '' : 'hide'}>Назад</div>
         </div>
     )
   }
 });
-var ClinicSelect = React.createClass({
+var ClinicOption = React.createClass({
   render: function() {
+    var selected = this.props.data.selected ? 'selected' : '';
+    if (selected) {
+      return (
+        <option value={this.props.data.id} selected>{this.props.data.title}</option>
+      )
+    }
     return (
-      <option value={this.props.data.id}>{this.props.data.title}</option>
-    )
+      <option value={this.props.data.id} >{this.props.data.title}</option>
+    );
   }
 });
 
 var PageContent = React.createClass({
+  onChangeSelect: function(e) {
+    e.preventDefault();
+    this.setState({clinicId: e.target.value});
+  },
   nextButtonClick: function(e) {
     e.preventDefault();
-    ee.emit('Buttons.next');
+    ee.emit('Buttons.next', {clinicId: this.state.clinicId});
+  },
+  getInitialState: function() {
+    return {
+      clinicId: Object.keys(clinics)[0]
+    }
   },
   render: function() {
+    var self = this;
+    var pageToRender = this.props.pageNum;
+    console.log('PageContent will render for page #' + pageToRender);
+
+    var pageTemplate;
+    switch (pageToRender) {
+      case 1:
+        pageTemplate = <div className="form" id="form">
+          <div className="formField">
+            <label htmlFor="clinic" className="formLabel">Выберите поликлинику:</label>
+            <select id="clinic" onChange={this.onChangeSelect}>
+              {Object.keys(clinics).map(function (key, index) {
+                var item = clinics[key];
+                item.id = key;
+                item.selected = (key === self.props.selectedClinicId);
+
+                return (
+                  <ClinicOption key={key} data={item}/>
+                )
+              })}
+            </select>
+          </div>
+
+          <div className="formField">
+            <button id="selectClinic" className="buttonNext" onClick={this.nextButtonClick}>Далее</button>
+          </div>
+        </div>;
+        break;
+
+      case 2:
+        pageTemplate = <b>Template page #{pageToRender}</b>;
+        break;
+      default:
+        pageTemplate = <b>Page num {pageToRender} template not defined</b>;
+        break;
+    }
+
     return(
         <div id="content" className="content">
-          <div className="form" id="form">
-            <div className="formField">
-              <label htmlFor="clinic" className="formLabel">Выберите поликлинику:</label>
-              <select id="clinic">
-                {Object.keys(clinics).map(function(key, index) {
-                  var item = clinics[key];
-                  item.id = key;
-                  return (
-                    <ClinicSelect key={key} data={item} />
-                  )
-                })}
-              </select>
-            </div>
-
-            <div className="formField">
-              <button id="selectClinic" className="buttonNext" onClick={this.nextButtonClick}>Далее</button>
-            </div>
-          </div>
+          {pageTemplate}
         </div>
     );
   }
@@ -52,24 +93,47 @@ var PageContent = React.createClass({
 var App = React.createClass({
   getInitialState: function() {
     return {
-      page: 1
+      page: 1,
+      clinicId: undefined
     };
   },
   componentDidMount: function() {
     var self = this;
-    window.ee.addListener('Buttons.next', function() {
-      self.setState({page: ++self.state.page});
+    window.ee.addListener('Buttons.next', function(data) {
+      console.log('data: ', data);
+
+      var pageNum = self.state.page + 1;
+      var state = {page: pageNum};
+
+      switch (self.state.page) {
+        case 1:
+          state.clinicId = data.clinicId;
+          break;
+        case 2:
+          break;
+        default:
+          break;
+      }
+      self.setState(state);
       console.log('event Buttons.next catched!', self.state.page);
+    });
+
+    window.ee.addListener('Buttons.back', function(data) {
+      console.log('data: ', data);
+      self.setState({page: self.state.page - 1});
+      console.log('event Buttons.back catched!', self.state.page);
     });
   },
   componentWillUnmount: function() {
-    window.ee.removeListener('News.add');
+    window.ee.removeListener('Buttons.next');
+    window.ee.removeListener('Buttons.back');
   },
   render: function() {
     return(
         <div>
-          <Nav />
-          <PageContent pageNum={this.state.page}/>
+          <Nav pageNum={this.state.page}/>
+          <PageContent pageNum={this.state.page}
+                       selectedClinicId={this.state.clinicId}/>
         </div>
     )
   }
